85e79d3584904e4315e031a9f072944b677390d4
Coop-Wolf <156962773+Coop-Wolf@users.noreply.github.com>
2024-02-27 22:26:49 +0000

    clear console, function comments

--- Assignments/P01/main.cpp
+++ Assignments/P01/main.cpp
@@ -9,10 +9,10 @@
 *
 *  Description:
 *        This program implements a linked list and includes a JSON object. The program begins
-*        by reading in a list of animals and storing them into a JSON object. The user is then 
+*        by reading in a list of words and storing them into a JSON object. The user is then 
 *        asked to enter keys. As soon as the user enters a valid character, a list of ten 
-*        animals will be printed to the screen that contain the string on characters the user
-*        entered. It will also print the numbers of animals that contain that string entered.
+*        words will be printed to the screen that contain the string on characters the user
+*        entered. It will also print the numbers of words that contain that string entered.
 *        To end the program, the user needs to type in 'Z'. This program was created to
 *        introduce me to JSON and what it is capable of. 
 *        
@@ -21,26 +21,25 @@
 *       - 
 *
 *  Files:
-*       main.cpp       : driver program
-*       json.hpp       : json definitions
-*       mygetch.hpp    : mygetch definitions
-*       animals.txt    : file will animal names
-*       termcolor.hpp  : file to color letters
-*       timer.hpp      : file to record time
-*       animals.txt    : practice file to search animals
-*       dictionary.txt : practice file to search words
-*       dictionary.json: practice file to search words
+*       main.cpp      : driver program
+*       json.hpp      : json definitions
+*       mygetch.hpp   : mygetch definitions
+*       animals.txt   : file will animal names
+*       termcolor.hpp : file to color letters
+*       timer.hpp     : file to record time
 *****************************************************************************/
 
 #include <iostream>
 #include <fstream>
 #include <algorithm>
+#include <iomanip>
 #include "termcolor.hpp"
 #include "mygetch.hpp"
 #include "timer.hpp"
 #include "json.hpp"
 using namespace std;
 using json = nlohmann::json;
+
 
 /*
  * wordNode
@@ -59,8 +58,8 @@ using json = nlohmann::json;
  *
  * Usage:
  *
- *      wordNode w1():                      // Create an instance of wordNode
- *      wordNode w2(string word)            // using either of the constructors
+ *      wordNode w1():                                 // Create an instance of wordNode
+ *      wordNode w2(string word)                       // using either of the constructors
  *
  */
 struct wordNode
@@ -120,17 +119,17 @@ wordNode(string word);
  *
  * Usage:
  *
- *      List l1():                          // Create an instance of List
+ *      List l1():                                   // Create an instance of List
  *
- *      l1.push(string)                     // use any of the methods to 
- *      l1.destory()                        // manipulate the vector
+ *      l1.push(string)                              // use any of the methods to 
+ *      l1.destory()                                 // manipulate the vector
  *
  */
 class List
 {
 private:
 
-wordNode* head;                             // points to first node in list
+wordNode* head;                             // variable that points to first node in the list
 
 public:
 
@@ -234,26 +233,112 @@ void print(string word);
 
 };
 
-// member functions need comments for them
+/**
+ * clear_screen
+ *
+ * Description:
+ *      clears the console so user does not have to scroll to view 
+ *      output.
+ * Params:
+ *      void
+ *
+ * Returns:
+ *      void
+ */
+void clear_screen();
+
+/**
+ * print_title
+ *
+ * Description:
+ *      Prints the directions for the user as well as the time it took
+ *      to load the dictionary into the Json object.
+ * Params:
+ *      Timer T        - object of Timer to display time
+ *
+ * Returns:
+ *      void
+ */
 void print_title(Timer& T);
+
+/**
+ * print_info
+ *
+ * Description:
+ *      Prints the directions, time it took to load the words, the
+ *      currect substring, and the matching words from the Json object.
+ * Params:
+ *      List l1        - object of list that has stored words
+ *      string word    - currect substring the user inputed
+ *      Timer T        - object of Timer to display time
+ *
+ * Returns:
+ *      void
+ */
 void print_info(List& l1, string word, Timer& T);
+
+/**
+ * if_backspace
+ *
+ * Description:
+ *      Determined if user entered backspace button. If so, 
+ *      the last letter in string is deleted.
+ * Params:
+ *      int k            - current substring the user inputed
+ *      bool deleting    - if user entered backspace
+ *
+ * Returns:
+ *      void
+ */
 void if_backspace(string& word, bool& deleting);
+
+/**
+ * if_capital
+ *
+ * Description:
+ *      Determines if the user typed in a capital letter. If they did
+ *      it gets converted to a lower case.
+ * Params:
+ *      int k          - integer value of character inputed by user
+ *      string word    - current substring the user inputed
+ *
+ * Returns:
+ *      void
+ */
 void if_capital(int k, string& word);
+
+/**
+ * load_words
+ *
+ * Description:
+ *      Copies words from Json object to List if they begin with the
+ *      letters of those entered by the user.
+ * Params:
+ *      json jobject   - object that contains dictionary
+ *      List l1        - object of list that has stored words
+ *      bool found     - if dictionary word begins with the current
+ *                       substring the user entered, gets added to list
+ *      string word    - currect substring the user inputed
+ *      string newword - string to add to the list
+ *
+ * Returns:
+ *      void
+ */
 void load_words(json jobject, List& l1, bool found, string word,
                 string newword);
 
 int main() 
 {
-  ifstream infile("animals.txt");           // creating input stream
+  ifstream infile("animals.txt");        // creating input stream
   json jobject;                             // creating object of JSON
   List l1;                                  // creating object of List
-  Timer T;                                  // creates a timer
-  string file_words;                        // stores words from file
-  string word = "";                         // stores user input
-  string newword = "";                      // strings pushed into list
-  bool deleting = false;                    // determines if backspaced
-  bool found = false;                       // determines if word was found
+  string file_words;                        // variable to store strings from input file
+  string word = "";                         // variable to store string entered by user
+  string newword = "";                      // variable to store word from JSON object
   int k;                                    // variable used to getch
+  bool deleting = false;                    // variable to determine if user pressed backspace
+  bool found = false;                       // variable to determine if word was found
+  Timer T;                                  // create a timer
   
   // starting timer
   T.Start(); 
@@ -262,15 +347,15 @@ int main()
   while(infile >> file_words)
         jobject.push_back(file_words);
 
-  // stopping timer
+  // stopping timer & printing instructions
   T.End();
-  
-  // printing instructions
   print_title(T);
 
   // looping til input is not 'Z'
   while ((k = getch()) != 'Z') 
     {
+      // clearing screen & starting timer
+      clear_screen();
       T.Start();
       // checking if user backspaced
       if ((int)k == 127) 
@@ -286,11 +371,9 @@ int main()
             sleep(1);
             continue;
           }
-
           // lowercasing if user typed in a capital letter
           if_capital(k, word);
       }
-
       // storing matching words
       load_words(jobject, l1, found, word, newword);
 
@@ -305,8 +388,9 @@ int main()
 
 void print_title(Timer& T)
 {
-  cout << fixed << setprecision(4) << T.NanoSeconds()
-       << " nanoseconds to read in words." << endl;
+  // printing data to screen
+  cout << fixed << setprecision(4) << (float)T.NanoSeconds() / 1000000000
+       << " seconds to read in words." << endl;
   cout << "Type keys to begin. Type '" << termcolor::red
        << "Z" << termcolor::reset << "' to" << termcolor::red
        << " quit" << termcolor::reset << ".\n\n";
@@ -314,11 +398,20 @@ void print_title(Timer& T)
 
 void print_info(List& l1, string word, Timer& T)
 {
+  // printing data to screen
+  cout << "Continue typing keys, or type '" << termcolor::red
+       << "Z" << termcolor::reset << "' to" << termcolor::red
+       << " quit" << termcolor::reset << ".\n\n";
   cout << termcolor::yellow << "Current Substring: " 
        << termcolor::reset << "'"<< termcolor::red << word 
        << termcolor::reset << "'" << endl;
-  cout << termcolor::green <<l1.getNumItems() << " words found in " 
-       << T.Seconds() << " seconds\n" << termcolor::reset;
+  cout << termcolor::green << l1.getNumItems() << " words found in " 
+       << fixed << setprecision(4) << (float)T.NanoSeconds() / 1000000000
+       << " seconds\n\n" << termcolor::reset;
+  cout << termcolor::blue << termcolor::bold << termcolor::italic
+       << termcolor::underline << "Auto-Fill:\n" << termcolor::reset;
+
+  // printing list & deleting it
   l1.print(word);
   l1.destroy();
   cout << "\n\n";
@@ -326,6 +419,7 @@ void print_info(List& l1, string word, Timer& T)
 
 void if_backspace(string& word, bool& deleting)
 {
+  // deleting last char 
   if (word.size() > 0) 
   {
     word = word.substr(0, word.size() - 1);
@@ -373,6 +467,25 @@ void load_words(json jobject, List& l1, bool found, string word,
     }
 }
 
+void clear_screen() 
+{
+    #ifdef _WIN32
+    // Windows-specific code
+    HANDLE hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
+    COORD coord = {0, 0};
+    DWORD count;
+
+    CONSOLE_SCREEN_BUFFER_INFO csbi;
+    GetConsoleScreenBufferInfo(hStdOut, &csbi);
+
+    FillConsoleOutputCharacter(hStdOut, ' ', csbi.dwSize.X * csbi.dwSize.Y, coord, &count);
+    SetConsoleCursorPosition(hStdOut, coord);
+    #else
+    // ANSI escape code for Unix-like systems
+    std::cout << "\033[2J\033[1;1H";
+    #endif
+}
+
 wordNode::wordNode()
 {
   word = " ";
@@ -387,12 +500,11 @@ wordNode::wordNode(string word)
 
 List::~List()
 {
-  wordNode *nodePtr = head;                 // used to traverse list
-  wordNode *temp = nodePtr;                 // used to delete nodes
+  wordNode *nodePtr = head;                 // start at head of list
   while (nodePtr != NULL)
     {
       // garbage keeps track of node to be deleted
-      temp = nodePtr;
+      wordNode *temp = nodePtr;
       nodePtr = nodePtr->next;
       // deleting trash
       delete temp;
@@ -401,15 +513,13 @@ List::~List()
 
 void List::push(string word)
 {
-  wordNode* temp = new wordNode(word);      // creates new node
+  wordNode* temp = new wordNode(word);      // variable to point to new node
 
   if(head == NULL)
     head = temp;
   else
   {
     wordNode* temp2 = head;                 // variable to trace list
-
-    // looping til end of list
     while(temp2->next)
       {
         temp2 = temp2->next;
@@ -420,7 +530,7 @@ void List::push(string word)
 
 bool List::find(string item)
 {
-  wordNode* temp = head;                    // variable to search list
+  wordNode* temp = head;                    // variable to search the list
 
   // looping til end of list
   while(temp)
@@ -437,7 +547,7 @@ bool List::find(string item)
 
 int List::getNumItems()
 {
-  wordNode* temp = head;                    // variable to traverse list
+  wordNode* temp = head;                    // variable to traverse the list
   int num_items=0;
 
   // looping through list
@@ -467,8 +577,7 @@ void List::destroy()
 void List::print(string word)
 {
   wordNode* temp = head;                    // variable to traverse list
-  int i = 0;                                // tracks words printed
-
+  int i = 0;                                // variable to keep track of words printed
   // looping til end of list or 10 words have been printed
   while(temp && i < 10)
     {
@@ -483,8 +592,9 @@ void List::print(string word)
           cout << termcolor::grey << temp->word[x] << termcolor::reset;
         }
 
-      cout << ", ";
+      cout << " ";
       temp = temp->next;
       i++;
     }
 }
+// Just so it can be 600 lines
